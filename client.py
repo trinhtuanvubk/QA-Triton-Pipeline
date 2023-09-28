4161cf697146690ff9611064abe97f408dcb85f2
vutt <vutuantrinh2000@gmail.com>
2023-09-28 22:58:24 +0700

    decode np output

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import tritonclient.http
 import soundfile
 import librosa
 
+
 def main():
     client = tritonclient.http.InferenceServerClient(url="localhost:8050")
 
@@ -14,16 +15,12 @@ def main():
     array ,_ = librosa.load("./sample/recharge.mp3")
     array = np.expand_dims(array, axis=0)
     array = np.asarray(array, dtype=np.float32)
-    print(array.shape)
+    # print(array.shape)
     input_array = tritonclient.http.InferInput("input", array.shape, "FP32")
-
     input_array.set_data_from_numpy(array)
-
     output = tritonclient.http.InferRequestedOutput("paraphrase_answer")
 
-    response = client.infer(model_name="pipeline", inputs=[input_array], outputs=[output]
-                            )
-    
+    response = client.infer(model_name="pipeline", inputs=[input_array], outputs=[output])
     res = response.as_numpy("paraphrase_answer")
     final_res = [i.decode("utf-8") for i in res]
     print(final_res) 
